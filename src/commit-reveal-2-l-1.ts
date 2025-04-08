dde6921ee520d8785b0eaa12b2a2c50c4f9757b8
DarkKnight3074 <aryan@tokamak.network>
2025-04-08 12:56:20 +0530

    update commit-reveal2.ts file

--- src/commit-reveal-2-l-1.ts
+++ src/commit-reveal-2-l-1.ts
@@ -1,19 +1,21 @@
+import { log } from "matchstick-as"
 import {
   Activated as ActivatedEvent,
   CoSubmitted as CoSubmittedEvent,
   CvSubmitted as CvSubmittedEvent,
   DeActivated as DeActivatedEvent,
   EIP712DomainChanged as EIP712DomainChangedEvent,
+  IsInProcess as IsInProcessEvent,
   L1FeeCalculationSet as L1FeeCalculationSetEvent,
   MerkleRootSubmitted as MerkleRootSubmittedEvent,
   OwnershipHandoverCanceled as OwnershipHandoverCanceledEvent,
   OwnershipHandoverRequested as OwnershipHandoverRequestedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   RandomNumberGenerated as RandomNumberGeneratedEvent,
+  RandomNumberRequested as RandomNumberRequestedEvent,
   RequestedToSubmitCo as RequestedToSubmitCoEvent,
   RequestedToSubmitCv as RequestedToSubmitCvEvent,
   RequestedToSubmitSFromIndexK as RequestedToSubmitSFromIndexKEvent,
-  Round as RoundEvent,
   SSubmitted as SSubmittedEvent
 } from "../generated/CommitReveal2L1/CommitReveal2L1"
 import {
@@ -22,18 +24,21 @@ import {
   CvSubmitted,
   DeActivated,
   EIP712DomainChanged,
+  IsInProcess,
   L1FeeCalculationSet,
   MerkleRootSubmitted,
   OwnershipHandoverCanceled,
   OwnershipHandoverRequested,
   OwnershipTransferred,
   RandomNumberGenerated,
+  RandomNumberRequested,
   RequestedToSubmitCo,
   RequestedToSubmitCv,
   RequestedToSubmitSFromIndexK,
-  Round,
-  SSubmitted
+  SSubmitted,
+  Round
 } from "../generated/schema"
+import { Bytes } from "@graphprotocol/graph-ts"
 
 export function handleActivated(event: ActivatedEvent): void {
   let entity = new Activated(
@@ -52,7 +57,7 @@ export function handleCoSubmitted(event: CoSubmittedEvent): void {
   let entity = new CoSubmitted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.co = event.params.co
   entity.index = event.params.index
 
@@ -67,7 +72,7 @@ export function handleCvSubmitted(event: CvSubmittedEvent): void {
   let entity = new CvSubmitted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.cv = event.params.cv
   entity.index = event.params.index
 
@@ -105,6 +110,19 @@ export function handleEIP712DomainChanged(
   entity.save()
 }
 
+export function handleIsInProcess(event: IsInProcessEvent): void {
+  let entity = new IsInProcess(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.isInProcess = event.params.isInProcess
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
 export function handleL1FeeCalculationSet(
   event: L1FeeCalculationSetEvent
 ): void {
@@ -126,7 +144,7 @@ export function handleMerkleRootSubmitted(
   let entity = new MerkleRootSubmitted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.merkleRoot = event.params.merkleRoot
 
   entity.blockNumber = event.block.number
@@ -134,6 +152,11 @@ export function handleMerkleRootSubmitted(
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  // only works for round 0
+  let round = Round.load("0") as Round;
+  round.merkleRootSubmitted = entity.id;
+  round.save();
 }
 
 export function handleOwnershipHandoverCanceled(
@@ -197,6 +220,35 @@ export function handleRandomNumberGenerated(
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let round = Round.load(event.params.round.toString()) as Round;
+  round.randomNumberGenerated = entity.id;
+  round.save();
+}
+
+export function handleRandomNumberRequested(
+  event: RandomNumberRequestedEvent
+): void {
+  let entity = new RandomNumberRequested(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.round = event.params.round
+  entity.timestamp = event.params.timestamp
+  entity.activatedOperators = changetype<Bytes[]>(
+    event.params.activatedOperators
+  )
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+
+  // Round
+  let round = new Round(event.params.round.toString());
+  round.round = event.params.round;
+  round.randomNumberRequested = entity.id;
+  round.save();
 }
 
 export function handleRequestedToSubmitCo(
@@ -205,7 +257,7 @@ export function handleRequestedToSubmitCo(
   let entity = new RequestedToSubmitCo(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.indices = event.params.indices
 
   entity.blockNumber = event.block.number
@@ -221,7 +273,7 @@ export function handleRequestedToSubmitCv(
   let entity = new RequestedToSubmitCv(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.indices = event.params.indices
 
   entity.blockNumber = event.block.number
@@ -237,22 +289,8 @@ export function handleRequestedToSubmitSFromIndexK(
   let entity = new RequestedToSubmitSFromIndexK(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.index = event.params.index
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRound(event: RoundEvent): void {
-  let entity = new Round(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.startTime = event.params.startTime
-  entity.state = event.params.state
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -265,7 +303,7 @@ export function handleSSubmitted(event: SSubmittedEvent): void {
   let entity = new SSubmitted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.startTime = event.params.startTime
+  entity.timestamp = event.params.timestamp
   entity.s = event.params.s
   entity.index = event.params.index
 
